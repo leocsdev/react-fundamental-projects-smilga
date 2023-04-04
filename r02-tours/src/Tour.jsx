0d197649e81657252efd3e3f81f38fb22e923be7
Leo <leocsdev@gmail.com>
2023-04-04 18:08:07 +0800

    L176 Read more functionality

--- r02-tours/src/Tour.jsx
+++ r02-tours/src/Tour.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        {/* <p>{info}</p> */}
+        <p>
+          {readMore ? info : `${info.substring(0, 150)}...`}
+          <br />
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
+
         <button
           type="button"
           className="btn btn-block delete-btn"
